d2e3f82713c29ab1355ad320ef6c09d58aa70362
Li Kai <likai@LimatoiMac.local>
2013-03-28 15:16:08 +0800

    Initial Commit

---
+++ MyUIView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyUIView
+//
+//  Created by Li Kai on 13-3-28.
+//  Copyright (c) 2013年 DomQiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
